a03dff335dbd75662375d6b237774e708a2b80ef
Deepak Chauhan <rishudc119@gmail.com>
2022-02-07 12:20:35 +0530

    Added comment to describe current cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,17 +16,17 @@ class ApplicationController < ActionController::Base
 
     def current_cart
       if session[:cart_id].present?
-        @current_cart = Cart.find_by(id: session[:cart_id])
+        @current_cart = Cart.find_by(id: session[:cart_id]) # current cart will be fetched from session in already there
       elsif current_user
-        if current_user.cart.present?
+        if current_user.cart.present? # set current cart from current user's cart if already present
           @current_cart = current_user.cart
           session[:cart_id] = @current_cart.id
         else
-          @current_cart = Cart.create(user_id: current_user.id)
+          @current_cart = Cart.create(user_id: current_user.id)  # create new cart if both the above case fails
           session[:cart_id] = @current_cart.id
         end
       else
-        @current_cart = nil
+        @current_cart = nil # if not logged_in then make them nil
         session[:cart_id] = nil
       end
     end
